056a3dd3c50145144013702a54d96ec7e02e13dc
adnan1naeem <adnan1naeem@gmail.com>
2021-10-11 09:52:19 +0500

    fix(added webCam packages): added webCam packages

--- src/pages/ready/ready.jsx
+++ src/pages/ready/ready.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React from "react";
+import Webcam from "react-webcam";
 
-const  Ready =()=>{
-        return <h1>Ready</h1>
-      }
-export  {Ready}
+const Ready = () => {
+  return (
+    <div>
+      <Webcam
+      // audio={false}
+      // height={720}
+      // ref={webcamRef}
+      // screenshotFormat="image/jpeg"
+      // width={1280}
+      // videoConstraints={videoConstraints}
+      />
+    </div>
+  );
+};
+export { Ready };
